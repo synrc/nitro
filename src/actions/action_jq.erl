cebdc55b6abc09a54b3748394c8633f7f6cd7fdf
m-2k <andy.y2k@gmail.com>
2016-10-07 22:54:38 +0300

    Any type of args allowed

--- src/actions/action_jq.erl
+++ src/actions/action_jq.erl
@@ -11,7 +11,7 @@
 
 render_action(#jq{property=?U,target=T,method=Methods,args=Args0,format=F}) ->
     Args = case F of "'~s'" -> [ ?R(Args0) ]; _ -> Args0 end,
-    Format = fun(A) when is_tuple(A) orelse is_integer(A) -> ?R(A); (A) -> A end,
+    Format = fun(A) when is_tuple(A) orelse is_integer(A) -> ?R(A); (A) -> nitro:to_list(A) end,
     RenderedArgs = string:join([ Format(A) || A <- Args], ","),
     [[?T(T),".",?B(M),"(",nitro:f(F,[RenderedArgs]),");"] || M <- Methods];
 render_action(#jq{target=T,method=?U,property=P,right=R,args=simple}) -> [?B(T),".",?B(P),"='",?R(R),"';"];
