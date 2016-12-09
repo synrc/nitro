4d6fbf931f15ff3cbe7a858adc6d462625de409e
5HT <maxim@synrc.com>
2016-12-09 14:32:29 +0200

    fix element_panel

---
+++ src/elements/element_panel.erl
@@ -0,0 +1,13 @@
+-module(element_panel).
+-author('Maxim Sokhatsky').
+-include("nitro.hrl").
+-compile(export_all).
+
+render_element(Record) ->
+   wf_tags:emit_tag(<<"div">>, nitro:render(Record#panel.body),
+     lists:append([
+       [{<<"id">>,   Record#panel.id},
+       {<<"class">>, Record#panel.class},
+       {<<"style">>, Record#panel.style}],
+       Record#panel.data_fields,
+       Record#panel.aria_states])).
