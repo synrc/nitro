06dc4a5d2414be5830f8c1d625999155c70e1ffc
Namdak Tonpa <maxim@synrc.com>
2017-05-01 23:34:50 +0300

    nitro mqtt

--- src/render/wf_render.erl
+++ src/render/wf_render.erl
@@ -5,15 +5,15 @@
 render_item(undefined) -> <<>>;
 render_item(E) when element(2,E) =:= element -> wf_render_elements:render_element(E);
 render_item(E) when element(2,E) =:= action  ->
-    io:format("Render Action: ~p~n",[E]),
+%    io:format("Render Action: ~p~n",[E]),
     wf_render_actions:render_action(E);
 render_item(E) ->
-    io:format("Render Item: ~p~n",[E]),
+%    io:format("Render Item: ~p~n",[E]),
     E.
 
 render(undefined) -> <<>>;
 render(<<E/binary>>) -> E;
 render(Elements) when is_list(Elements) ->
-    io:format("Render: ~p~n",[Elements]),
+%    io:format("Render: ~p~n",[Elements]),
    [ render_item(E) || E <- Elements, E /= undefined ];
 render(Elements) -> render_item(Elements).
