2238f7f239b71792793526481eaaf0abc29af354
Namdak Tonpa <maxim@synrc.com>
2018-11-22 12:54:04 +0200

    q qc nitro

--- src/nitro.erl
+++ src/nitro.erl
@@ -5,6 +5,10 @@
 -behaviour(application).
 -export([start/2, stop/1, init/1]).
 
+q(Key) -> Val = get(Key), case Val of undefined -> qc(Key); A -> A end.
+qc(Key) -> CX = get(context), qc(Key,CX).
+qc(Key,Ctx) -> proplists:get_value(nitro:to_binary([Key]),Ctx#cx.params).
+
 start(_StartType, _StartArgs) -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 stop(_State) -> ok.
 init([]) -> {ok, {{one_for_one, 5, 10}, []}}.
