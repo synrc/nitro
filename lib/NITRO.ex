be32055e7c3efa4829e135acb309a3cab8bc42b4
Namdak Tonpa <maxim@synrc.com>
2019-06-10 14:21:47 +0300

    fix bug

--- lib/NITRO.ex
+++ lib/NITRO.ex
@@ -1,10 +1,10 @@
 defmodule NITRO do
   require Record
   import  Record, only: [defrecord: 2, extract: 2]
-  defrecord :button,  extract(:button, from_lib: "nitro/include/nitro.hrl")
-  defrecord :upload,  extract(:upload, from_lib: "nitro/include/nitro.hrl")
-  defrecord :h2,      extract(:h2,     from_lib: "nitro/include/nitro.hrl")
-  defrecord :message, extract(:button, from_lib: "nitro/include/nitro.hrl")
-  defrecord :author,  extract(:upload, from_lib: "nitro/include/nitro.hrl")
-  defrecord :jq,      extract(:jq,     from_lib: "nitro/include/nitro.hrl")
+  defrecord :button,  extract(:button,  from_lib: "nitro/include/nitro.hrl")
+  defrecord :upload,  extract(:upload,  from_lib: "nitro/include/nitro.hrl")
+  defrecord :h2,      extract(:h2,      from_lib: "nitro/include/nitro.hrl")
+  defrecord :message, extract(:message, from_lib: "nitro/include/nitro.hrl")
+  defrecord :author,  extract(:author,  from_lib: "nitro/include/nitro.hrl")
+  defrecord :jq,      extract(:jq,      from_lib: "nitro/include/nitro.hrl")
 end
