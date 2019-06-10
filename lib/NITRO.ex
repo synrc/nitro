2cc113b175c5323b3fcf700e52da96f6f5fcb99a
Namdak Tonpa <maxim@synrc.com>
2019-06-10 13:44:41 +0300

    message authpr

--- lib/NITRO.ex
+++ lib/NITRO.ex
@@ -1,7 +1,9 @@
 defmodule NITRO do
   require Record
   import  Record, only: [defrecord: 2, extract: 2]
-  defrecord :button, extract(:button, from_lib: "nitro/include/nitro.hrl")
-  defrecord :upload, extract(:upload, from_lib: "nitro/include/nitro.hrl")
-  defrecord :h2,     extract(:h2,     from_lib: "nitro/include/nitro.hrl")
+  defrecord :button,  extract(:button, from_lib: "nitro/include/nitro.hrl")
+  defrecord :upload,  extract(:upload, from_lib: "nitro/include/nitro.hrl")
+  defrecord :h2,      extract(:h2,     from_lib: "nitro/include/nitro.hrl")
+  defrecord :message, extract(:button, from_lib: "nitro/include/nitro.hrl")
+  defrecord :author,  extract(:upload, from_lib: "nitro/include/nitro.hrl")
 end
