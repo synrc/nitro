0a781bbfe3fc31cbdb72afaa9b0bb3cd747e34c1
Namdak Tonpa <maxim@synrc.com>
2019-06-10 15:16:47 +0300

    alias

--- lib/NITRO.ex
+++ lib/NITRO.ex
@@ -1,6 +1,7 @@
 defmodule NITRO do
   require Record
   import  Record, only: [defrecord: 2, extract: 2]
+  alias :nitro, as: NITRO
   defrecord :button,  extract(:button,  from_lib: "nitro/include/nitro.hrl")
   defrecord :upload,  extract(:upload,  from_lib: "nitro/include/nitro.hrl")
   defrecord :h2,      extract(:h2,      from_lib: "nitro/include/nitro.hrl")
