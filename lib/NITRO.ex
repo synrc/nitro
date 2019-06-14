c3d525153180d8395629c0de2b1c5de88608dcc4
Namdak Tonpa <maxim@synrc.com>
2019-06-14 09:02:46 +0300

    promote all records to Elixir

--- lib/NITRO.ex
+++ lib/NITRO.ex
@@ -1,10 +1,7 @@
 defmodule NITRO do
   require Record
-  import  Record, only: [defrecord: 2, extract: 2]
-  defrecord :button,  extract(:button,  from_lib: "nitro/include/nitro.hrl")
-  defrecord :upload,  extract(:upload,  from_lib: "nitro/include/nitro.hrl")
-  defrecord :h2,      extract(:h2,      from_lib: "nitro/include/nitro.hrl")
-  defrecord :message, extract(:message, from_lib: "nitro/include/nitro.hrl")
-  defrecord :author,  extract(:author,  from_lib: "nitro/include/nitro.hrl")
-  defrecord :jq,      extract(:jq,      from_lib: "nitro/include/nitro.hrl")
+
+  Enum.each(Record.extract_all(from_lib: "nitro/include/nitro.hrl"), fn {name, definition} ->
+    Record.defrecord(name, definition)
+  end)
 end
--- mix.exs
+++ mix.exs
@@ -2,11 +2,7 @@ defmodule NITRO.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :nitro,
-     version: "4.4.0",
-     description: "NITRO Web Framework and HTML5 DSL",
-     package: package(),
-     deps: deps()]
+    [app: :nitro, version: "4.4.0", description: "NITRO Web Framework and HTML5 DSL", package: package(), deps: deps()]
   end
 
   def application do
@@ -14,14 +10,16 @@ defmodule NITRO.Mixfile do
   end
 
   def package do
-    [files: ["src", "include", "priv", "LICENSE", "README.md", "rebar.config" ],
-     licenses: ["DHARMA"],
-     maintainers: ["Namdak Tonpa"],
-     name: :nitro,
-     links: %{"GitHub" => "https://github.com/synrc/nitro"}]
+    [
+      files: ["src", "include", "priv", "LICENSE", "README.md", "rebar.config"],
+      licenses: ["DHARMA"],
+      maintainers: ["Namdak Tonpa"],
+      name: :nitro,
+      links: %{"GitHub" => "https://github.com/synrc/nitro"}
+    ]
   end
 
   def deps do
-     [{:ex_doc, ">= 0.0.0", only: :dev}]
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 end
