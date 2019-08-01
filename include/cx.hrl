f40756277bda681bb3eae33bd77639b3c9cfcc70
Namdak Tonpa <maxim@synrc.com>
2019-08-01 17:11:24 +0300

    wip

--- include/cx.hrl
+++ include/cx.hrl
@@ -2,6 +2,7 @@
 -define(NITRO_CX, true).
 
 -record(cx, { handlers=[], actions=[], req=[], module=[], lang=[], path=[],
-              session=[], formatter=bert, params=[], node=[], client_pid=[], state=[], from=[], vsn = [] }).
+              session=[], token=[], formatter=bert, params=[], node=[],
+              client_pid=[], state=[], from=[], vsn = [] }).
 
 -endif.
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "4.7.4",
+      version: "4.7.7",
       description: "NITRO Web Framework",
       package: package(),
       deps: deps()
