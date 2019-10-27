dfa432f6b164d8bf0bef3dc6d6aa1740a8e18fba
Namdak Tonpa <maxim@synrc.com>
2019-10-27 05:42:36 +0200

    onmouseover onmouseout for tr

--- src/elements/table/element_tr.erl
+++ src/elements/table/element_tr.erl
@@ -13,5 +13,7 @@ render_element(Record = #tr{postback= Postback}) ->
     {<<"id">>, Id},
     {<<"onclick">>, Record#tr.onclick},
     {<<"class">>, Record#tr.class},
+    {<<"onmouseover">>, Record#tr.onmouseover},
+    {<<"onmouseout">>, Record#tr.onmouseout},
     {<<"style">>, [Record#tr.style, Cursor]} | Record#tr.data_fields
   ]).
