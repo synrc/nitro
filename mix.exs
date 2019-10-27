78297dd9318df1cb12cf46294e9cc334489ebbfc
Namdak Tonpa <maxim@synrc.com>
2019-10-27 04:30:01 +0200

    onmouseout

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "4.10.5",
+      version: "4.10.6",
       description: "NITRO Web Framework",
       package: package(),
       deps: deps()
