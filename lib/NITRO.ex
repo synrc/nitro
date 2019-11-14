b051b5e1b12f5b656f740ce9212bb7fef19830c3
Namdak Tonpa <maxim@synrc.com>
2019-11-14 03:24:16 +0200

    4.11.5

--- lib/NITRO.ex
+++ lib/NITRO.ex
@@ -1,8 +1,33 @@
 defmodule NITRO do
   require Record
 
-  Enum.each(Record.extract_all(from_lib: "nitro/include/nitro.hrl"), fn {name,
-                                                                         definition} ->
-    Record.defrecord(name, definition)
-  end)
+  files = Path.wildcard("include/**/*")
+
+  hrl_files =
+    Enum.filter(files, fn f ->
+      !String.contains?(f, "/_") and Path.extname(f) == ".hrl"
+    end)
+
+  Enum.each(
+    hrl_files,
+    fn t ->
+      Enum.each(
+        Record.extract_all(from_lib: "nitro/" <> t),
+        fn {name, definition} ->
+          IO.inspect({name, definition, t})
+          prev = :application.get_env(:kernel, :nitro_tables, [])
+
+          case :lists.member(name, prev) do
+            true ->
+              :skip
+
+            false ->
+              Record.defrecord(name, definition)
+              :application.set_env(:kernel, :nitro_tables, [name | prev])
+          end
+        end
+      )
+    end
+  )
+
 end
