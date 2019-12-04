cac04c4ba80d1aa8c8936a53a6e81c34e2e5beea
pal-alex <palchikovskiy.alexander@gmail.com>
2019-12-04 16:21:08 +0200

    gitignore

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "4.11.12",
+      version: "4.11.13",
       description: "NITRO Nitrogen Web Framework",
       package: package(),
       deps: deps()
