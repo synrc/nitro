acb85276f3ed4be5d35b3c19a6baa3a37a10af9d
Oleksandr Naumov <oleksandr.naumov91@gmail.com>
2019-12-23 20:24:20 +0200

    4.12.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "4.12.5",
+      version: "4.12.6",
       description: "NITRO Nitrogen Web Framework",
       package: package(),
       deps: deps()
