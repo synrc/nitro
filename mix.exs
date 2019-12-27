3b092da7d4c14b03c4a74cef584775152479a2d6
pal-alex <34629649+pal-alex@users.noreply.github.com>
2019-12-27 13:17:44 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "4.12.7",
+      version: "4.12.8",
       description: "NITRO Nitrogen Web Framework",
       package: package(),
       deps: deps()
