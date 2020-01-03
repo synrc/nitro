11407498601d201b8235e7c89e09570abe73310a
5HT <maxim@synrc.com>
2020-01-03 16:15:03 +0200

    5.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "4.12.11",
+      version: "5.1.1",
       description: "NITRO Nitrogen Web Framework",
       package: package(),
       deps: deps()
