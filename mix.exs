8dfd77090db92ae7fb8fe1ae45316de300a8e57c
SunRiseGC <SunRiseGC4@gmail.com>
2021-04-07 20:27:21 +0300

    fix validation for comboLookupEdit

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "5.12.4",
+      version: "5.4.0",
       description: "NITRO Nitrogen Web Framework",
       package: package(),
       deps: deps()
