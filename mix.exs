b5ac4b91f08af1593dbe0bbb8c6d1cd0d0c6b383
SunRiseGC <SunRiseGC4@gmail.com>
2021-06-02 13:10:18 +0300

    version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "6.6.0",
+      version: "6.6.1",
       description: "NITRO Nitrogen Web Framework",
       package: package(),
       deps: deps()
