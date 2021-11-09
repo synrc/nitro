ee5a651591a1d7362557ddccf15f068efe7d6c8e
Namdak Tonpa <maxim@synrc.com>
2021-11-09 18:49:33 +0200

    6.11.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "6.11.6",
+      version: "6.11.7",
       description: "NITRO Nitrogen Web Framework",
       package: package(),
       deps: deps()
