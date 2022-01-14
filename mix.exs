ea8d0336da03793a6bd8c688499740db87956e39
dxt <doxtop@synrc.com>
2022-01-14 14:47:01 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "6.12.0",
+      version: "7.1.0",
       description: "NITRO Nitrogen Web Framework",
       package: package(),
       deps: deps()
