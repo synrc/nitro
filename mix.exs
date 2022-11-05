541b5ee1156c1c736fc3f5d91d819a2d80d7ae0f
རྣམ་དག་སྟོན་པ <maxim@synrc.com>
2022-11-05 05:52:16 +0000

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "7.9.3",
+      version: "7.10.0",
       description: "NITRO Nitrogen Web Framework",
       package: package(),
       deps: deps()
