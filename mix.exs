835c993d2414a01a1eade8dd76333a9ddb25a976
dxt <doxtop@synrc.com>
2023-03-03 15:34:48 +0200

    mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NITRO.Mixfile do
   def project do
     [
       app: :nitro,
-      version: "8.2.2",
+      version: "8.2.3",
       description: "NITRO Nitrogen Web Framework",
       package: package(),
       deps: deps()
